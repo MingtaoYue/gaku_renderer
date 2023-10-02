219535e370753e52a23f99e02a8cc898d9799d5b
MingtaoYue <gakumeitou0317@gmail.com>
2023-10-02 15:58:04 +0900

    draw triangle

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
-const int width = 2000;
-const int height = 2000;
+const TGAColor blue = TGAColor(0, 0, 255, 255);
+const int width = 500;
+const int height = 500;
 Model *model = NULL;
 
 // Line drawing function (Bresenham's line drawing algorithm).
@@ -47,28 +48,35 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0.x, t0.y, t1.x, t1.y, image, color);
+    line(t1.x, t1.y, t2.x, t2.y, image, color);
+    line(t2.x, t2.y, t0.x, t0.y, image, color);
+}
+
 int main(int argc, char** argv) {
-    model = new Model("obj/mvs3d.obj");
     TGAImage image(width, height, TGAImage::RGB);
-    
+    Vec2i triangle0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
+    Vec2i triangle1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
+    Vec2i triangle2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(triangle0[0], triangle0[1], triangle0[2], image, red);
+    triangle(triangle1[0], triangle1[1], triangle1[2], image, white);
+    triangle(triangle2[0], triangle2[1], triangle2[2], image, blue);
     // Draw the model.
-    for (int i = 0; i < model->nfaces(); i++) {
-        vector<int> face = model->face(i);
-        for (int j = 0; j < 3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            // Original range is [-1, 1], scale to [0, width], [0, height].
-            // int x0 = (v0.x + 1) * width / 2;
-            // int y0 = (v0.y + 1) * height / 2;
-            // int x1 = (v1.x + 1) * width / 2;
-            // int y1 = (v1.y + 1) * height / 2;
-            int x0 = v0.x * 5 + 1000;
-            int y0 = -v0.y * 5+ 1000;
-            int x1 = v1.x * 5+ 1000;
-            int y1 = -v1.y * 5+ 1000;
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
+    // model = new Model("obj/diablo3_pose.obj");
+    // for (int i = 0; i < model->nfaces(); i++) {
+    //     vector<int> face = model->face(i);
+    //     for (int j = 0; j < 3; j++) {
+    //         Vec3f v0 = model->vert(face[j]);
+    //         Vec3f v1 = model->vert(face[(j + 1) % 3]);
+    //         // Original range is [-1, 1], scale to [0, width], [0, height].
+    //         int x0 = (v0.x + 1) * width / 2;
+    //         int y0 = (v0.y + 1) * height / 2;
+    //         int x1 = (v1.x + 1) * width / 2;
+    //         int y1 = (v1.y + 1) * height / 2;
+    //         line(x0, y0, x1, y1, image, white);
+    //     }
+    // }
 
     // Origin at the left bottom corner of the image.
     image.flip_vertically();
