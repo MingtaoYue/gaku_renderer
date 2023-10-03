f3edfde23a3e10e3bf3dadb16ea73ea655d19878
MingtaoYue <gakumeitou0317@gmail.com>
2023-10-03 18:35:05 +0900

    simple lighting with back-face culling.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
-const int width = 1000;
-const int height = 1000;
+const int width = 10000;
+const int height = 10000;
 
 Model *model = NULL;
 
@@ -54,15 +54,24 @@ int main(int argc, char** argv) {
     // triangle(triangle0, image, red);
 
     // Draw the model.
-    model = new Model("obj/diablo3_pose.obj");
+    model = new Model("obj/african_head.obj");
+    Vec3f light_dir(0, 0, -1);
+
     for (int i = 0; i < model->nfaces(); i++) {
         vector<int> face = model->face(i);
         Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
             Vec3f v = model->vert(face[j]);
             screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
         }
-        triangle(screen_coords, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]); 
+        n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        }   
     }
 
     // Origin at the left bottom corner of the image.
