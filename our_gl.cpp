564956998fa106491502cd89712d399b69dde5ab
MingtaoYue <gakumeitou0317@gmail.com>
2023-11-09 16:54:38 +0900

    revise draw triangle method

--- our_gl.cpp
+++ our_gl.cpp
@@ -39,3 +39,43 @@ vec3 barycentric(const vec2 tri[3], const vec2 P) {
         return {-1, 1, 1};
     return M.invert_transpose() * embed<3>(P);
 }
+
+// draw triangle
+void triangle(const vec4 clip_verts[3], IShader &shader, TGAImage &image, std::vector<double> &zbuffer) {
+    // canonical frustum -> screen space
+    vec4 pts[3] = {Viewport * clip_verts[0], Viewport * clip_verts[1], Viewport * clip_verts[2]};
+    // 3d homogeneous -> 2d cartesian
+    vec2 pts_xy[3] = {proj<2>(pts[0] / pts[0][3]), proj<2>(pts[1] / pts[1][3]), proj<2>(pts[2] / pts[2][3])};
+
+    // set bounding box of the triangle
+    int bboxmin[2] = {image.width() - 1, image.height() - 1};
+    int bboxmax[2] = {0, 0};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            bboxmin[j] = std::min(bboxmin[j], static_cast<int>(pts_xy[i][j]));
+            bboxmax[j] = std::max(bboxmax[j], static_cast<int>(pts_xy[i][j]));
+        }
+    }
+
+    #pragma omp parallel for
+    // iterate over each pixel in the bounding box
+    for (int x = std::max(0, bboxmin[0]); x <= std::min(bboxmax[0], image.width() - 1); x++) {
+        for (int y = std::max(0, bboxmin[1]); y <= std::min(bboxmax[1], image.height() - 1); y++) {
+            // barycentric coordinates of the pixel
+            vec3 bc = barycentric(pts_xy, {static_cast<double>(x), static_cast<double>(y)});
+            // interpolated depth of the pixel
+            double frag_depth = vec3{pts[0][2] / pts[0][3], pts[1][2] / pts[1][3], pts[2][2] / pts[2][3]} * bc;
+            // if the pixel is not inside the triangle (either of the barycentric coordinates is negative), or the depth is smaller than the zbuffer, then skip
+            if (bc.x < 0 || bc.y < 0 || bc.z < 0 || frag_depth < zbuffer[x + y * image.width()])
+                continue;
+            
+            TGAColor color;
+            // fragment shader can discard the pixel
+            if (shader.fragment(bc, color))
+                continue;
+            // update zbuffer with current depth
+            zbuffer[x + y * image.width()] = frag_depth;
+            image.set(x, y, color);
+        }
+    }
+}
